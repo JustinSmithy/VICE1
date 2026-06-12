01846850eb61f860d1fd710aee717dbda3412d44
JustinSmithy <75529070+JustinSmithy@users.noreply.github.com>
2026-06-12 23:19:43 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,17 +1,9 @@
-// ========================================
-// SUPABASE CONFIG
-// ========================================
-
 window.SUPABASE_URL = "https://twdgwnbvpuwjwbbjfack.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_LzF2eh8Nvf8iGX0-5_izKw_7yuwV0Hw";
 
-// ========================================
-// SUPABASE CLIENT
-// ========================================
-
 const { createClient } = supabase;
 
-const sb = createClient(
-  window.SUPABASE_URL,
-  window.SUPABASE_ANON_KEY
+window.sb = createClient(
+window.SUPABASE_URL,
+window.SUPABASE_ANON_KEY
 );
