8452daf369f193e3ad1148f83861fb5b7d1634d2
JustinSmithy <75529070+JustinSmithy@users.noreply.github.com>
2026-06-18 15:16:32 +0100

    Delete js directory

--- js/config.js
+++
@@ -1,3 +0,0 @@
-window.SUPABASE_URL = "https://twdgwnbvpuwjwbbjfack.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_LzF2eh8Nvf8iGX0-5_izKw_7yuwV0Hw";
-window.sb = supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
